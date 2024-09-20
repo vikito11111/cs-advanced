a68a5ce2de54f642fb1668e6fb4dfdaaeb066b72
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-09-20 16:13:05 +0300

    Add files via upload

---
+++ C# - Exercising/02. Multidimensional Arrays/01. Sum Matrix Elements/01. Sum Matrix Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿var firstInput = Console.ReadLine()
+    .Split(separator: new string[] { ", " }, options: StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int n = firstInput[0];
+int m = firstInput[1];
+
+int[,] matrix = new int[n, m];
+
+var matrixSum = 0;
+
+for (int row = 0; row < n; row++)
+{
+    var secondInput = Console.ReadLine()
+        .Split(separator: new string[] { ", " }, options: StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < m; col++)
+    {
+        matrix[row, col] = secondInput[col];
+        matrixSum += secondInput[col];
+    }
+}
+
+Console.WriteLine(matrix.GetLength(0));
+Console.WriteLine(matrix.GetLength(1));
+Console.WriteLine(matrixSum);
+
+Console.ReadLine();
---
+++ C# - Exercising/02. Multidimensional Arrays/02. Sum Matrix Columns/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,45 @@
+﻿var matrixSizeInput = Console.ReadLine()
+    .Split(separator: new string[] { ", " },options: StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int n = matrixSizeInput[0];
+int m = matrixSizeInput[1];
+
+var matrix = new int[n, m];
+
+FillMatrix(n, m, matrix);
+
+PrintMatrixColsSums(m, n, matrix);
+
+static void PrintMatrixColsSums(int m, int n, int[,] matrix)
+{
+    for (int col = 0; col < m; col++)
+    {
+        var colSum = 0;
+
+        for (int row = 0; row < n; row++)
+        {
+
+            colSum += matrix[row, col];
+        }
+
+        Console.WriteLine(colSum);
+    }
+}
+
+static void FillMatrix(int n, int m, int[,] matrix)
+{
+    for (int row = 0; row < n; row++)
+    {
+        var matrixElementsInput = Console.ReadLine()
+            .Split(separator: " ")
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int col = 0; col < m; col++)
+        {
+            matrix[row, col] = matrixElementsInput[col];
+        }
+    }
+}
