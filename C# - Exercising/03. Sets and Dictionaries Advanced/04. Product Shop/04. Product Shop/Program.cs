81d462dd6065476563def01f68313444d1dcbf75
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-09-21 17:35:54 +0300

    Add files via upload

---
+++ C# - Exercising/03. Sets and Dictionaries Advanced/04. Product Shop/04. Product Shop/Program.cs
@@ -0,0 +1,33 @@
+﻿var shopDictionary = new Dictionary<string, Dictionary<string, double>>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Revision")
+{
+    var inputTokens = input.Split(separator: new[] { ", " },options: StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    string shop = inputTokens[0];
+    string product = inputTokens[1];
+    double price = double.Parse(inputTokens[2]);
+
+    if (shopDictionary.ContainsKey(shop))
+    {
+        shopDictionary[shop].Add(product, price);
+    }
+    else
+    {
+        shopDictionary.Add(shop, new Dictionary<string, double>());
+        shopDictionary[shop].Add(product, price);
+    }
+}
+
+foreach (var shop in shopDictionary.OrderBy(x => x.Key))
+{
+    Console.WriteLine($"{shop.Key}->");
+
+    foreach (var product in shop.Value)
+    {
+        Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+    }
+}
