f4ab7717b9d995a4d4e8f70ee35c11b76b686a13
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-02 17:27:39 +0300

    Add files via upload

---
+++ C# - Exercising/06. Defining Classes/01. Car/01. Car/Program.cs
@@ -0,0 +1,44 @@
+﻿var car = new Car("Toyota", "Camry", 2020);
+
+car.DisplayInfo();
+
+public class Car
+{
+    // Private fields:
+    private string make;
+    private string model;
+    private int year;
+
+    // Public properties with full get and set methods:
+    public string Make
+    {
+        get => make;
+        set => make = value;
+    }
+
+    public string Model
+    {
+        get => model;
+        set => model = value;
+    }
+
+    public int Year
+    {
+        get => year;
+        set => year = value;
+    }
+
+    // Constructor:
+    public Car(string make, string model, int year)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+    }
+
+    // Methods:
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}");
+    }
+}
