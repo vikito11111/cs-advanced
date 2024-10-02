22568da2d175f7cd7507209a9ee76a46002603f5
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-02 17:28:45 +0300

    Add files via upload

---
+++ C# - Exercising/06. Defining Classes/03. Car Constructors/03. Car Constructors/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Car_Constructors
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make 
+        { 
+            get => make; 
+            set => make = value; 
+        }
+
+        public string Model 
+        { 
+            get => model; 
+            set => model = value; 
+        }
+
+        public int Year 
+        { 
+            get => year; 
+            set => year = value;
+        }
+
+        public double FuelQuantity 
+        { 
+            get => fuelQuantity; 
+            set => fuelQuantity = value; 
+        }
+
+        public double FuelConsumption 
+        { 
+            get => fuelConsumption; 
+            set => fuelConsumption = value; 
+        }
+
+        public Car()
+        {
+            Make = "VW";
+
+            Model = "Golf";
+
+            Year = 2025;
+
+            FuelQuantity = 200;
+
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : 
+            this()
+        {
+            Make = make;
+
+            Model = model;
+
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : 
+            this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+
+            FuelConsumption = fuelConsumption;
+        }
+    }
+}
---
+++ C# - Exercising/06. Defining Classes/03. Car Constructors/03. Car Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using _03._Car_Constructors;
+
+string make = Console.ReadLine();
+string model = Console.ReadLine();
+int year = int.Parse(Console.ReadLine());
+double fuelQuantity = double.Parse(Console.ReadLine());
+double fuelConsumption = double.Parse(Console.ReadLine());
+
+var firstCar = new Car();
+
+var secondCar = new Car(make, model, year);
+
+var thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
