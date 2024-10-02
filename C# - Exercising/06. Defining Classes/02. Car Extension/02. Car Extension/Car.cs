facae77c35eed4dda4021897ea36f3b2b857e7bb
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-02 17:28:10 +0300

    Add files via upload

---
+++ C# - Exercising/06. Defining Classes/02. Car Extension/02. Car Extension/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Car_Extension
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make 
+        { 
+            get => make; 
+            set => make = value; 
+        }
+
+        public string Model 
+        { 
+            get => model; 
+            set => model = value;
+        }
+
+        public int Year 
+        { 
+            get => year; 
+            set => year = value; 
+        }
+
+        public double FuelQuantity 
+        { 
+            get => fuelQuantity; 
+            set => fuelQuantity = value; 
+        }
+
+        public double FuelConsumption 
+        { 
+            get => fuelConsumption; 
+            set => fuelConsumption = value;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public void Drive(double distance)
+        {
+            var consumption = distance * this.FuelConsumption / 100.0;
+
+            if (this.FuelQuantity > consumption)
+            {
+                this.FuelQuantity -= consumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return ($"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L");
+        }
+    }
+}
---
+++ C# - Exercising/06. Defining Classes/02. Car Extension/02. Car Extension/Program.cs
@@ -0,0 +1,7 @@
+﻿using _02._Car_Extension;
+
+var car = new Car("VW", "MK3", 1992, 200, 200);
+
+car.Drive(2000);
+
+Console.WriteLine(car.WhoAmI());
