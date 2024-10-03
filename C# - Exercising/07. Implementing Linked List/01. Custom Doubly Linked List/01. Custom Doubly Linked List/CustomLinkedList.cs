85a3f1c718a15c60a3a809b044e518534b94de40
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-03 17:57:22 +0300

    Add files via upload

---
+++ C# - Exercising/07. Implementing Linked List/01. Custom Doubly Linked List/01. Custom Doubly Linked List/CustomLinkedList.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Custom_Doubly_Linked_List
+{
+    public class CustomLinkedList
+    {
+        public Node Head { get; set; }
+
+        public Node Tail { get; set; }
+
+        public int Count { get; set; }
+
+        public void AddFirst(int value)
+        {
+            Count++;
+
+            Node node = new Node(value);
+
+            if (Head == null)
+            {
+                Head = node;
+                Tail = node;
+
+                return;
+            }
+
+            Head.Previous = node;
+
+            node.Next = Head;
+            Head = node;
+        }
+
+        public void AddLast(int value)
+        {
+            Count++;
+
+            Node node = new Node(value);
+
+            if (Tail == null)
+            {
+                Head = node;
+                Tail = node;
+
+                return;
+            }
+
+            Tail.Next = node;
+
+            node.Previous = Tail;
+            Tail = node;
+        }
+
+        public Node RemoveFirst()
+        {
+            if (Head == null)
+            {
+                return null;
+            }
+
+            Count--;
+
+            Node oldHead = Head;
+
+            if (Head.Next == null)
+            {
+                Head = null;
+                Tail = null;
+
+                return oldHead;
+            }
+
+            Node newHead = Head.Next;
+
+            Head.Next = null;
+
+            newHead.Previous = null;
+
+            Head = newHead;
+
+            return oldHead;
+        }
+
+        public Node RemoveLast()
+        {
+            if (Tail == null)
+            {
+                return null;
+            }
+
+            Count--;
+
+            Node oldTail = Tail;
+
+            if (Tail.Previous == null)
+            {
+                Head = null;
+                Tail = null;
+
+                return oldTail;
+            }
+
+            Node newTail = Tail.Previous;
+
+            Tail.Previous = null;
+
+            newTail.Next = null;
+
+            Tail = newTail;
+
+            return oldTail;
+        }
+
+        public void ForEach(Action<Node> action)
+        {
+            Node node = Head;
+
+            while (node != null)
+            {
+                action(node);
+
+                node = node.Next;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+
+            int index = 0;
+
+            ForEach(x =>
+            {
+                array[index++] = x.Value;
+            });
+
+            return array;
+        }
+    }
+}
---
+++ C# - Exercising/07. Implementing Linked List/01. Custom Doubly Linked List/01. Custom Doubly Linked List/Program.cs
@@ -0,0 +1,46 @@
+﻿//using _01._Custom_Doubly_Linked_List;
+using _01._Custom_Doubly_Linked_List;
+
+CustomLinkedList linkedList = new CustomLinkedList();
+
+linkedList.AddLast(1);
+linkedList.AddLast(2);
+linkedList.AddLast(3);
+linkedList.AddLast(4);
+linkedList.AddLast(5);
+
+linkedList.ForEach(x =>
+{
+    Console.WriteLine($"ForEach is cool -> {x.Value}");
+});
+
+int[] array = linkedList.ToArray();
+
+Console.WriteLine($"{string.Join(" ", array)}");
+
+/*var ten = new Node(10);
+var twenty = new Node(20);
+var thirty = new Node(30);
+var fourty = new Node(40);
+
+Node head = new Node(10,
+                new Node(20,
+                    new Node(30,
+                        new Node(40))));
+
+Node currentNode = head;
+
+ten.Next = twenty;
+twenty.Previous = ten;
+
+twenty.Next = thirty;
+thirty.Previous = twenty;
+
+thirty.Next = fourty;
+fourty.Previous = thirty;
+
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Next;
+}*/
