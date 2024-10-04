cf1fac3daec1c2900687845a7b21617fd947d3f3
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-04 16:44:18 +0300

    Add files via upload

---
+++ C# - Exercising/08. Generics/03. Generic Count Method Double/Box.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Generic_Count_Method_Double
+{
+    public class Box<T>
+        where T: IComparable
+    {
+        public List<T> List { get; set; }
+
+        public Box()
+        {
+            List = new List<T>();
+        }
+
+        public void AddElement(T element)
+        {
+            List.Add(element);
+        }
+
+        public int CompareByValue(T value)
+        {
+            int count = 0;
+
+            foreach (var element in List)
+            {
+                if (element.CompareTo(value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C# - Exercising/08. Generics/03. Generic Count Method Double/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Generic_Count_Method_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+
+                box.AddElement(element);
+            }
+
+            double valueForCompare = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(box.CompareByValue(valueForCompare));
+
+            Console.ReadLine();
+        }
+    }
+}
