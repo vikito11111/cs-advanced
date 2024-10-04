1483c9c2aed5fa96ae64f02899949d57626e0a68
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-04 16:45:29 +0300

    Add files via upload

---
+++ C# - Exercising/08. Generics/04. Generic Swap Method String/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Generic_Swap_Method_String
+{
+    public class Box<T>
+    {
+        public List<T> List { get; set; }
+
+        public Box()
+        {
+            List = new List<T>();
+        }
+
+        public void AddElement(T element)
+        {
+            List.Add(element);
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            T swapElement = List[index1];
+
+            List[index1] = List[index2];
+
+            List[index2] = swapElement;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var element in List)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# - Exercising/08. Generics/04. Generic Swap Method String/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Generic_Swap_Method_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.AddElement(input);
+            }
+
+            string[] indexInput = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            int index1 = int.Parse(indexInput[0]);
+
+            int index2 = int.Parse(indexInput[1]);
+
+            box.Swap(index1, index2);
+
+            Console.WriteLine(box.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
