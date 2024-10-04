cb2b14b82134a6efc64a913e15bbb22a5d4fedc4
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-04 16:33:00 +0300

    Add files via upload

---
+++ C# - Exercising/08. Generics/01. Box of T/01. Box of T/Box.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Box_of_T
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        private int Count 
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (Count <= 0)
+            {
+                throw new Exception("Cannot remove an element in an empty Box!");
+            }
+            else
+            {
+                T elementToRemove = list.Last();
+
+                list.Remove(elementToRemove);
+
+                return elementToRemove;
+            }
+        }
+    }
+}
---
+++ C# - Exercising/08. Generics/01. Box of T/01. Box of T/Program.cs
@@ -0,0 +1,24 @@
+﻿using _01._Box_of_T;
+
+var box = new Box<int>();
+
+box.Add(1);
+box.Add(2);
+box.Add(3);
+
+Console.WriteLine(box.Remove());
+
+box.Add(4);
+box.Add(5);
+
+Console.WriteLine(box.Remove());
+
+Console.WriteLine("--------------------------------");
+
+var box2 = new Box<string>();
+
+box2.Add("one");
+box2.Add("two");
+box2.Add("three");
+
+Console.WriteLine(box2.Remove());
