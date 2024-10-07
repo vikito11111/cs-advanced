9cde0bf02e31a38c69c6e329cd9122ebe997f5d5
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-07 19:08:01 +0300

    Add files via upload

---
+++ C# - Exercising/08. Generics/08. Threeuple/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPersonInfo = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string fullName = inputPersonInfo[0] + " " + inputPersonInfo[1];
+
+            string address = inputPersonInfo[2];
+
+            string town = string.Join(" ", inputPersonInfo.Skip(3));
+
+            Threeuple<string, string, string> personInfo = new Threeuple<string, string, string>(fullName, address, town);
+
+
+            string[] inputBeerInfo = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
+
+            string name = inputBeerInfo[0];
+
+            int litersOfBeer = int.Parse(inputBeerInfo[1]);
+
+            //bool isDrunk = inputBeerInfo[2] == "drunk" ? true : false;
+
+            bool drunkCheck = false;
+
+            if (inputBeerInfo[2] == "drunk")
+            {
+                drunkCheck = true;
+            }
+
+            Threeuple<string, int, bool> beerInfo = new Threeuple<string, int, bool>(name, litersOfBeer, drunkCheck);
+
+
+            string[] bankInfoInput = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string personName = bankInfoInput[0];
+
+            double bankBalance = double.Parse(bankInfoInput[1]);
+
+            string bankName = bankInfoInput[2];
+
+            Threeuple<string, double, string> bankInfo = new Threeuple<string, double, string>(personName, bankBalance, bankName);
+
+            Console.WriteLine(personInfo);
+
+            Console.WriteLine(beerInfo);
+
+            Console.WriteLine(bankInfo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# - Exercising/08. Generics/08. Threeuple/Threeuple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<T, TT, TTT>
+    {
+        public Threeuple(T item1, TT item2, TTT item3)
+        {
+            Item1 = item1;
+
+            Item2 = item2;
+
+            Item3 = item3;
+        }
+
+        public T Item1 { get; set; }
+
+        public TT Item2 { get; set; }
+
+        public TTT Item3 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+    }
+}
