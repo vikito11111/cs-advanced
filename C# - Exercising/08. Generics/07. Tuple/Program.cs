ba1ec4512bf999c066eede8a37faf15bbc887370
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-07 19:07:35 +0300

    Add files via upload

---
+++ C# - Exercising/08. Generics/07. Tuple/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPersonInfo = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string fullName = inputPersonInfo[0] + " " + inputPersonInfo[1];
+
+            string address = inputPersonInfo[2];
+
+            Tuple<string, string> personInfo = new Tuple<string, string>(fullName, address);
+
+
+            string[] inputBeerInfo = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
+
+            string name = inputBeerInfo[0];
+
+            int litersOfBeer = int.Parse(inputBeerInfo[1]);
+
+            Tuple<string, int> personBeer = new Tuple<string, int>(name, litersOfBeer);
+
+        
+            string[] inputNumberInfo = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            int myInt = int.Parse(inputNumberInfo[0]);
+
+            double myDouble = double.Parse(inputNumberInfo[1]);
+
+            Tuple<int, double> numbersInfo = new Tuple<int, double>(myInt, myDouble);
+
+            Console.WriteLine(personInfo.ToString());
+
+            Console.WriteLine(personBeer.ToString());
+
+            Console.WriteLine(numbersInfo.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# - Exercising/08. Generics/07. Tuple/Tuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Tuple
+{
+    public class Tuple<T, K>
+    {
+        public Tuple(T item1, K item2)
+        {
+            Item1 = item1;
+
+            Item2 = item2;
+        }
+
+        public T Item1 { get; set; }
+
+        public K Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
