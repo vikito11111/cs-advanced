ba4168481116a6b8fbbc57f58f59e149e4ac726e
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-11 14:28:06 +0300

    Add files via upload

---
+++ C# - Exercising/09. Iterators and Compalators/02. Library - Advanced/Book.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Library
+{
+    public class Book : IComparable<Book>
+    {
+        //public Book(string title, int year, params string[] authors)
+
+        public Book(string title, int year, decimal price, List<string> authors)
+        {
+            Title = title;
+
+            Year = year;
+
+            Price = price;
+
+            Authors = authors;
+
+            //Authors = authors.ToList();
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year > other.Year)
+            {
+                return 1;
+            }
+            else if (this.Year < other.Year)
+            {
+                return -1;
+            }
+            else
+            {
+                //return this.Title.CompareTo(other.Title); //Ако не ми се пишат if-ове
+
+                if (this.Title.Length > other.Title.Length)
+                {
+                    return 1;
+                }
+                else if (this.Title.Length < other.Title.Length)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public string Title { get; private set; }
+
+        public int Year { get; private set; }
+
+        public List<string> Authors { get; private set; }
+
+        public decimal Price { get; set; }
+    }
+}
